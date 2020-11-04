59d35149635f9306b8d63274c66be6c803c716b4
Andrew Popp <apopp524@gmail.com>
2020-11-03 20:44:44 -0600

    added react-bootstrap, moved to pages folder, and started some code for the login/sign up

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Navbar } from './components/Navbar/Navbar';
-import { Home } from './components/Home/Home';
+import { Navbar } from './pages/Navbar/index';
+import { Home } from './pages/Home/index';
 
 function App() {
   return (
