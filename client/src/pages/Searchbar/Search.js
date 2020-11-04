436533f174cbbc6dcffd9b66f03276153dea3258
cermqm <63324300+cermqm@users.noreply.github.com>
2020-11-04 15:01:07 -0600

    updated API/Search with additional fields - flight numbers, airline name and duration

--- client/src/pages/Searchbar/Search.js
+++ client/src/pages/Searchbar/Search.js
@@ -2,15 +2,17 @@
 // import ResultsList from "./ResultsList"
 const axios = require("axios");
 
-const Search = async(datefrom, dateto, fromairport, toairport, callback) => {
+const Search = async(returnfrom, dateto, fromairport, toairport, callback) => {
 
-    let searchurl = "https://tequila-api.kiwi.com/v2/search?&curr=USD"
+    let searchurl = "https://tequila-api.kiwi.com/v2/search?&curr=USD&select_airlines=DL,B6,WN,AS,HA,UA,NK,AA,F9&flight_type=round"
     let apikey = "dRfNfRXhvDHSsgF7Got2L96r1cNGF9gl"
-    let date_from = "&date_from=" + datefrom
+    let return_from = "&return_from=" + returnfrom
+    let return_to = "&return_to=" + returnfrom
     let date_to = "&date_to=" + dateto
+    let date_from = "&date_from=" + dateto
     let fly_from = "&fly_from=airport:" + fromairport
     let fly_to = "&fly_to=airport:" + toairport
-    let surl = searchurl+date_from+date_to+fly_from+fly_to
+    let surl = searchurl+date_to+date_from+return_to+return_from+fly_from+fly_to
 
     axios({
         "method":"GET",
@@ -22,7 +24,7 @@ const Search = async(datefrom, dateto, fromairport, toairport, callback) => {
         .then((response)=>{
             // console.log("url = ", {surl})
             // console.log("headers = ", {apikey})
-            console.log("this is the response from the promise = ", response.data.data)
+            console.log("this is the response from the promise = ", response.data.data);
             callback(response.data.data);
             // console.log("searchresults = ", searchresults)
 
