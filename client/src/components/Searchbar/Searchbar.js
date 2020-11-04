195a5ca94851777690f7e181f99ebe0c6d3c621d
cermqm <63324300+cermqm@users.noreply.github.com>
2020-11-04 14:24:14 -0600

    added some additional fields to output - flight numbers, converted airline code to name and Duration.

--- client/src/components/Searchbar/Searchbar.js
+++ client/src/components/Searchbar/Searchbar.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Search from "./Search"
+import ConvertAirline from "./ConvertAirline";
+import CalculateDuration from "./CalculateDuration";
 // const axios = require("axios");
 import "./style.css"
 
@@ -8,26 +10,26 @@ const SearchBar = () => {
 
     const [fromairport, setfromairport] = useState('');
     const [toairport, settoairport] = useState('');
-    const [datefrom, setdatefrom] = useState('');
+    const [returnto, setreturnto] = useState('');
     const [dateto, setdateto] = useState('');
     const [searchresults, setsearchresults] = useState(null)
 
     useEffect(() => {
         const timeoutID = setTimeout(() => {
-            if (fromairport && toairport && dateto) {
+            if (fromairport && toairport && dateto && returnto) {
                 console.log(fromairport)
                 console.log(toairport)
-                console.log(datefrom)
+                console.log(returnto)
                 console.log(dateto)
-                Search(datefrom, dateto, fromairport, toairport, setsearchresults);
+                Search(returnto, dateto, fromairport, toairport, setsearchresults);
             }
         }, 750);
         return () => {
             clearTimeout(timeoutID);
         };
-    }, [fromairport, toairport, datefrom, dateto]);
+    }, [fromairport, toairport, dateto, returnto]);
 
-        console.log({searchresults})
+        // if ({searchresults} !== 'null') console.log({searchresults})
 
         return (
             <div className="ui segment searchbar">
@@ -49,7 +51,7 @@ const SearchBar = () => {
                         />
                         <br />
                         <br />
-                        <label>Date To </label>
+                        <label>Departure Date </label>
                         <input 
                             value={dateto} 
                             onChange={(e) => setdateto(e.target.value)} 
@@ -57,10 +59,10 @@ const SearchBar = () => {
                         />
                         <br />
                         <br />
-                        <label>Date From </label>
+                        <label>Return Date </label>
                         <input 
-                            value={datefrom} 
-                            onChange={(e) => setdatefrom(e.target.value)} 
+                            value={returnto} 
+                            onChange={(e) => setreturnto(e.target.value)} 
                             className="input"
                         />
 
@@ -70,12 +72,35 @@ const SearchBar = () => {
                     {
                         searchresults &&
                         searchresults.map( result => {
-                            return <li key={result.id}>
-                                {result.airlines} - 
-                                {result.flyFrom} - 
-                                {result.flyTo} - 
-                                {result.price} - 
+                            // console.log("result.airline[0] = ", result.airlines[0]);
+                            let airlinename = ConvertAirline(result.airlines[0]);
+                            // console.log("airlinename = ", airlinename);
+                            let ddh = CalculateDuration(result.duration.departure);
+                            let rdh = CalculateDuration(result.duration.return);
+                            let flightnumbers = "";
+                            for (let i = 0; i < result.route.length; i++) {
+                                // console.log("flightnumbers = ", flightnumbers);
+                                flightnumbers = flightnumbers + result.route[i].operating_flight_no + "/" ;
+                                // console.log("flightnumbers = ", flightnumbers);
+                            }
+                            
+                            if (ddh === -1 || rdh === -1) {
+                                // console.log("in if statement");
+                                // console.log("ddh = ", ddh);
+                                // console.log("rdh = ", rdh);
+                                // console.log("toooo long");
+                                return
+                            } else {
+                                return <li key={result.id}>
+                                {flightnumbers} - 
+                                {airlinename} -  
+                                {result.flyFrom} -  
+                                {result.flyTo} -  
+                                ${result.price} -  
+                                {ddh} -  
+                                {rdh}
                                 </li>
+                            }
                         })
                     }
                 </ul>
