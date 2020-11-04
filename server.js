a467b5e654b4a3008610373081f7a6ca6bdf3948
Josiah Powell <josiahpowell@outlook.com>
2020-11-03 23:10:25 -0600

    adding seed function for server.js

--- server.js
+++ server.js
@@ -28,6 +28,11 @@ require("./controllers")(app);
 // =============================================================
 db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+    seed();
+    console.log("http://localhost:" + PORT);
   });
 });
+
+const seed = () => {
+  db.user
+}
