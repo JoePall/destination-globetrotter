c0c7b64ea8ae05f0ada391ed83af907b601e09bd
Josiah Powell <josiahpowell@outlook.com>
2020-11-05 17:18:33 -0600

    Adding API through react router

--- controllers/index.js
+++ controllers/index.js
@@ -1,65 +1,64 @@
+const router = require("express").Router();
 const db = require("../models");
-const isAuthenticated = require("../config/middleware");
 
-module.exports = function (app) {
-  const setupGet = (basePath, model) => {
-    console.log("Constructing GET: " + basePath);
-    app.get(basePath, isAuthenticated, (req, res) => {
-      db[model].findAll().then(data => {
-        res.json(data);
-      });
+const setupGet = (basePath, model) => {
+  console.log("Constructing GET: " + basePath);
+  router.route(path).get(() => {
+    db[model].findAll().then(data => {
+      res.json(data);
     });
-
-    return { "get": basePath };
-  } 
+  });
   
-  const setupGetOne = (basePath, model) => {
-    const path = basePath + ":id";
-    console.log("Constructing GET: " + path);
-    app.get(path, isAuthenticated, (req, res) => {
-      db[model].findAll({ where: { id: req.params.id } }).then(data => {
-        res.json(data);
-      });
-    });
+  return { "get": basePath };
+} 
 
-    return { "getOne": path };
-  } 
+const setupGetOne = (basePath, model) => {
+  const path = basePath + ":id";
+  console.log("Constructing GET: " + path);
+  router.route(path).get(() => {
+    db[model].findAll({ where: { id: req.params.id } }).then(data => {
+      res.json(data);
+    });
+  });
   
-  const setupCreate = (basePath, model) => {
-    const path = basePath;
-    console.log("Constructing PUT: " + path);
-    app.put(path, isAuthenticated, (req, res) => {
-      db[model].create(req.body).then(data => {
-        res.json(data);
-      });
-    });
+  return { "getOne": path };
+} 
 
-    return { "create": basePath };
-  } 
+const setupCreate = (basePath, model) => {
+  const path = basePath;
+  console.log("Constructing PUT: " + path);
+  router.route(path).put(() => {
+    db[model].create(req.body).then(data => {
+      res.json(data);
+    });
+  });
   
-  const setupUpdate = (basePath, model) => {
-    const path = basePath + ":id";
-    console.log("Constructing PUT: " + path);
-    app.put(path, isAuthenticated, (req, res) => {
-      let result = db.findOne({ id: req.params.id });
+  return { "create": basePath };
+} 
 
-      Object.keys(req.body).forEach(key => {
-        result[key] = req.body[key];
-      });
-
-      db[model].update(result,
-        { where: { id: req.params.id } }).then(data => {
+const setupUpdate = (basePath, model) => {
+  const path = basePath + ":id";
+  console.log("Constructing PUT: " + path);
+  router.route(path).put(() => {
+    let result = db.findOne({ id: req.params.id });
+    
+    Object.keys(req.body).forEach(key => {
+      result[key] = req.body[key];
+    });
+    
+    db[model].update(result,
+      { where: { id: req.params.id } }).then(data => {
         res.json(data);
       });
     });
-
+    
     return { "update": path };
   } 
   
   const setupDelete = (basePath, model) => {
     const path = basePath + ":id";
     console.log("Constructing GET: " + path);
-    app.delete(path, isAuthenticated, (req, res) => {
+    router.route(path).delete(() => {
       db[model].destroy({ where: { id: req.params.id } });
     });
     
@@ -68,20 +67,18 @@ module.exports = function (app) {
   
   const setupAPI = (routes) => {
     let API = {};
-
+    
     routes.forEach(route => {
       Object.keys(route).forEach(key => {
         API.prototype[key] = routes[key];
       });
     });
-
-    app.get("/api/", (req, res) => {
-      res.json(API);
-    });
+    
+    router.route("/api/").get(res.json(API));
   }
-
+  
   let routes = [];
-
+  
   Object.keys(db).filter(x => x.toLowerCase == "sequelize").forEach(model => {
     const basePath = "/api/" + model + "/"; 
     
@@ -90,10 +87,8 @@ module.exports = function (app) {
     routes.push(setupUpdate(basePath, model));
     routes.push(setupGetOne(basePath, model));
     routes.push(setupDelete(basePath, model));
-  });
+});
 
-  setupAPI(routes);
+setupAPI(routes);  
 
-  module.exports = app;
-};
-
+module.exports = router;
