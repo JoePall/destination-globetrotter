2897822605a02ff0cfb5380cc15abf362e8d07cf
Josiah Powell <josiahpowell@outlook.com>
2020-11-06 14:58:42 -0600

    Adding config and more routing

--- controllers/index.js
+++ controllers/index.js
@@ -1,97 +1,84 @@
-const router = require("express").Router();
-const db = require("../models");
+const db = require("../controllers");
 
-const setupGet = (basePath, model) => {
-  const path = basePath;
-  console.log("Constructing GET: " + basePath);
-  router.route(path).get(() => {
-    db[model].findAll().then(data => {
-      res.json(data);
-    });
-  });
-  
-  return { "get": path };
-} 
-
-const setupGetOne = (basePath, model) => {
-  const path = basePath + ":id";
-  console.log("Constructing GET: " + path);
-  router.route(path).get(() => {
-    db[model].findAll({ where: { id: req.params.id } }).then(data => {
-      res.json(data);
-    });
-  });
-  
-  return { "getOne": path };
-} 
-
-const setupCreate = (basePath, model) => {
-  const path = basePath;
-  console.log("Constructing PUT: " + path);
-  router.route(path).put(() => {
-    db[model].create(req.body).then(data => {
-      res.json(data);
-    });
-  });
-  
-  return { "create": path };
-} 
-
-const setupUpdate = (basePath, model) => {
-  const path = basePath + ":id";
-  console.log("Constructing PUT: " + path);
-  router.route(path).put(() => {
-    let result = db.findOne({ id: req.params.id });
-    
-    Object.keys(req.body).forEach(key => {
-      result[key] = req.body[key];
-    });
-    
-    db[model].update(result,
-      { where: { id: req.params.id } }).then(data => {
+module.exports = function(app) {
+  const setupGet = (basePath, model) => {
+    const path = basePath;
+    console.log("Constructing GET: " + basePath);
+    app.get(path, (req, res) => {
+      console.log(path);
+      db[model].findAll().then(data => {
         res.json(data);
       });
     });
     
+    return { "get": path };
+  } 
+
+  const setupGetOne = (basePath, model) => {
+    const path = basePath + ":id";
+    console.log("Constructing GET: " + path);
+    app.get(path, (req, res) => {
+      console.log(path);
+      db[model].findAll({ where: { id: req.params.id } }).then(data => {
+        res.json(data);
+      });
+    });
+    
+    return { "getOne": path };
+  } 
+
+  const setupCreate = (basePath, model) => {
+    const path = basePath;
+    console.log("Constructing PUT: " + path);
+    app.put(path, (req, res) => {
+      console.log(path);
+      db[model].create(req.body).then(data => {
+        res.json(data);
+      });
+    });
+    
+    return { "create": path };
+  }
+
+  const setupUpdate = (basePath, model) => {
+    const path = basePath + ":id";
+    console.log("Constructing PUT: " + path);
+    
+    
     return { "update": path };
   } 
-  
+    
   const setupDelete = (basePath, model) => {
     const path = basePath + ":id";
     console.log("Constructing GET: " + path);
-    router.route(path).delete(() => {
+    app.delete(path, (req, res) => {
       db[model].destroy({ where: { id: req.params.id } });
     });
     
     return { "delete": path };
   }
-  
-  const setupAPI = (routes) => {
-    let API = {};
-    
-    routes.forEach(route => {
-      Object.keys(route).forEach(key => {
-        API.prototype[key] = routes[key];
-      });
-    });
-    
-    router.route("/api/").get(() => {
-      res.json(API);
-    });
-  }
-  
+
   let routes = [];
   
   Object.keys(db).filter(x => x.toLowerCase == "sequelize").forEach(model => {
     const basePath = "/api/" + model + "/"; 
-    
+    console.log(basePath);
     routes.push(setupGet(basePath, model));
     routes.push(setupCreate(basePath, model));
     routes.push(setupUpdate(basePath, model));
     routes.push(setupGetOne(basePath, model));
     routes.push(setupDelete(basePath, model));
-});
+  });
+  
+  let API = {};
 
-setupAPI(routes);  
-
-module.exports = router;
+  routes.forEach(route => {
+    Object.keys(route).forEach(key => {
+      API.prototype[key] = routes[key];
+    });
+  });
+  
+  app.get("/api/", (req, res) => {
+    res.json(API);
+  });
+}
