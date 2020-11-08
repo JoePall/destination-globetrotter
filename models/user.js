eccbc337e4ead3572301f23139609ad08b5312b8
Julie Schaub <julie.schaub@hotmail.com>
2020-11-08 10:21:22 -0600

    added connection and group dbs

--- models/user.js
+++ models/user.js
@@ -43,5 +43,11 @@ module.exports = function (sequelize, DataTypes) {
     user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
   });
 
+  User.associate = function (models) {
+    User.hasMany(models.Groups, {
+      onDelete: 'cascade',
+    });
+  };
+
   return User;
 };
