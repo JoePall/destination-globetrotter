ee6ff62821d931292629d74ca40f0a2fb8184e7f
cermqm <63324300+cermqm@users.noreply.github.com>
2020-11-09 13:04:11 -0600

    added some formatting for date/time and flight detail...

--- client/src/pages/Search-Flights/DisplayFlight.js
+++ client/src/pages/Search-Flights/DisplayFlight.js
@@ -2,6 +2,7 @@ import React from "react";
 import Card from 'react-bootstrap/Card';
 import ConvertAirline from "./ConvertAirline";
 import CalculateDuration from "./CalculateDuration";
+import ConvertDateTime  from "./ConvertDateTime";
 // import Button from 'react-bootstrap/Table'
 
 
@@ -11,6 +12,7 @@ function DisplayFlight(props) {
     let duration = null;
     let ddh = CalculateDuration(duration=durationdeparture);
     let rdh = CalculateDuration(duration=durationreturn);
+    let datetime = null;
 
     // let i=0;
     if (!props.result && props.result.route.length === 0) return (null)
@@ -21,15 +23,15 @@ function DisplayFlight(props) {
                 <Card.Body>
                     {props.result.route.map((route, i) => (
                         <div key={i}>
-                            <Card.Title>Flight Detail</Card.Title>
-                                <Card.Text>
+                            <Card.Title className="cardtitle">Flight Detail</Card.Title>
+                                <Card.Text className="cardtext">
                                     <div>{ConvertAirline(props.result.route[i].airline)} - Flight #{props.result.route[i].flight_no}</div>
-                                    <div>{props.result.route[i].cityFrom} -- Local Departure - {props.result.route[i].local_departure}</div>
-                                    <div>{props.result.route[i].cityTo} -- Local Arrival - {props.result.route[i].local_arrival}</div>
+                                    <div>{props.result.route[i].cityFrom} -- Local Departure - {ConvertDateTime(datetime = props.result.route[i].local_departure)}</div>
+                                    <div>{props.result.route[i].cityTo} -- Local Arrival - {ConvertDateTime(datetime = props.result.route[i].local_arrival)}</div>
                                     <div>{(props.result.cityCodeFrom === props.result.route[i].cityCodeFrom) ? ddh : ""}</div>
                                     <div>{(props.result.cityCodeTo === props.result.route[i].cityCodeFrom) ? rdh : ""}</div>
+                                    <div className="flightdetaildivider">{(props.result.cityCodeTo === props.result.route[i].cityCodeTo) ? "****************************" : ""}</div>
                                 </Card.Text>
-                            <div>----------------------------------------------------</div>
                         </div>
                     ))}
                 </Card.Body>
