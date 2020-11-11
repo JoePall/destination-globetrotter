e6d62e416f425dc4b02ab729d0bf6aed82cfc87b
Julie Schaub <julie.schaub@hotmail.com>
2020-11-10 18:47:35 -0600

    updated models > bookmarks.js

--- models/bookmarks.js
+++ models/bookmarks.js
@@ -1,27 +1,6 @@
 module.exports = function (sequelize, DataTypes) {
     var Bookmarks = sequelize.define('Bookmarks', {
-      bookmarkOne: {
-        type: DataTypes.TEXT,
-        allowNull: false,
-        validate: {
-          len: [1],
-        },
-      },
-      bookmarkTwo: {
-        type: DataTypes.TEXT,
-        allowNull: false,
-        validate: {
-          len: [1],
-        },
-      },
-      bookmarkThree: {
-        type: DataTypes.TEXT,
-        allowNull: false,
-        validate: {
-          len: [1],
-        },
-      },
-      bookmarkFour: {
+      tripName: {
         type: DataTypes.TEXT,
         allowNull: false,
         validate: {
@@ -29,12 +8,6 @@ module.exports = function (sequelize, DataTypes) {
         },
       },
     });
-  
-    Bookmarks.associate = function (models) {
-      Bookmarks.hasMany(models.User, {
-        onDelete: 'cascade',
-      });
-    };
   
     return Bookmarks;
   };
