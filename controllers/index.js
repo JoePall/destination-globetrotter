523565f21381e47bc8aa97ee5425281cdcf2b52d
Josiah Powell <josiahpowell@outlook.com>
2020-11-12 09:41:30 -0600

    Finishing Controllers

--- controllers/index.js
+++ controllers/index.js
@@ -5,59 +5,56 @@ const db = require("../models");
 const setupGet = (basePath, model) => {
   const path = basePath;
   router
-    .route(path)
-    .get((req, res) => {
+    .get(path, (req, res) => {
     db[model].findAll().then(data => {
       res.json(data);
     });
   });
 
-  return [ "get", path, "get", "" ];
+  return { operation: "get", path: path, type: "get" };
 }
   
 const setupGetOne = (basePath, model) => {
   const path = basePath + ":id";
-  router.route(path).get((req, res) => {
+  router.get(path, (req, res) => {
     db[model].findAll({ where: { id: req.params.id } }).then(data => {
       res.json(data);
     });
   });
   
-  return [ "getOne", path, "get", "id" ];
+  return { operation: "getOne", path: path, type: "get", params: ["id"] };
 } 
 
 const setupCreate = (basePath, model) => {
   const path = basePath;
-  router.route(path).post((req, res) => {
+  router.post(path, (req, res) => {
     db[model].create(req.body).then(data => {
       res.json(data);
     });
   });
   
-  return [ "create", path, "post", "o" ];
+  return { operation: "create", path: path, type: "post", object: ["o"] };
 }
 
 const setupUpdate = (basePath, model) => {
   const path = basePath + ":id";
 
-  return [ "update", path, "put" ];
+  router.put(path, (req, res) => {
+    db[model].update(req.body, { where: { id: req.params.id } }).then(data => {
+      res.json(data);
+    });
+  });
+
+  return { operation: "update", path: path, type: "put", params: ["id"], object: ["o"] };
 } 
-  
+
 const setupDelete = (basePath, model) => {
   const path = basePath + ":id";
   router.route(path).delete((req, res) => {
     db[model].destroy({ where: { id: req.params.id } });
   });
   
-  return [ "delete", path, "delete", "id" ];
-}
-
-const setupAPIRoutes = (routes) => {
-  let API = { api: routes };
-  
-  router.route("/api/").get((req, res) => {
-    res.json(API);
-  });
+  return { method: "delete", path: path, type: "delete", params: ["id"] };
 }
 
 module.exports = (models) => {
@@ -66,7 +63,7 @@ module.exports = (models) => {
   Object.keys(models).forEach(model => {
     if (model) {
       const basePath = "/api/" + model.toLowerCase() + "/"; 
-
+      
       routes = addToAPIObject(routes, model, setupGet(basePath, model));
       routes = addToAPIObject(routes, model, setupCreate(basePath, model));
       routes = addToAPIObject(routes, model, setupGetOne(basePath, model));
@@ -74,15 +71,43 @@ module.exports = (models) => {
       routes = addToAPIObject(routes, model, setupDelete(basePath, model));
     }
   });
-
-  setupAPIRoutes(routes);
+  
+  router.get("/api/", (req, res) => {
+    res.json({ api: routes });
+  });
+  
+  setupCustomRoutes();
   
   return router;
 }
 
-function addToAPIObject(routes, model, o) {
-  if (!routes[model]) routes[model] = {};
-  routes[model][o[0]] = "(" + o[3] + ") => axios." + o[2] + "(" + o[1] + ")";
+function addToAPIObject(routes, model, { operation, path, type, params, object }) {
+  let name = model.toLowerCase(); 
+  if (!routes[name]) routes[name] = [];
+  let result = operation + ": (";
+  if (params) result += params.join(", ");
+  if (params && object) result += ", ";
+  if (object) result += object.join(", ");
+  result += ") => axios." + type + "(\"" + path + "\"";
+  if (params) result += ", " + params.join(", ");
+  if (object) result += ", " + object.join(", ");
+  result += ")";
+  routes[name].push(result)
 
   return routes;
+}
+
+function setupCustomRoutes() {
+  router.get("/api/authenticated/", (req, res) => {
+    if (req.user) res.send(true);
+    else res.send(false);
+  });
+
+  router.post("/api/login", (req, res) => {
+    console.log("Hello from /api/login");
+    res.json({
+      email: req.user.email,
+      id: req.user.id
+    });
+  });
 }
