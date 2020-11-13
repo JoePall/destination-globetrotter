4a5c74e4dc7c1cf3dfea75660dc20a2f319547c4
Josiah Powell <josiahpowell@outlook.com>
2020-11-13 13:55:41 -0600

    Implementing 24 hr timeout to log out user

--- server.js
+++ server.js
@@ -14,18 +14,21 @@ app.use(express.json());
 app.use(express.static("public"));
 
 app.use(
-  session({ secret: "Expresso in Hawaii, whilst walking by the shore!", resave: true, saveUninitialized: true })
+  session({
+    secret: "Espresso in Tahiti Mornings! Fresh Seafood in Hawaii evenings!",
+    resave: true,
+    saveUninitialized: true,
+    cookie: { maxAge: 86400000 }
+  })
 );
 app.use(passport.initialize());
 app.use(passport.session());
 
-
 db.sequelize.sync({ force: true }).then((seq) => {
-  
   // Setting up route controllers for db.
   app.use(require("./controllers")(seq.models));
   app.use(require("./routes/html-routes"));
-  
+
   app.listen(PORT, function () {
     seed();
     console.log("http://localhost:" + PORT);
@@ -33,6 +36,16 @@ db.sequelize.sync({ force: true }).then((seq) => {
 });
 
 const seed = () => {
-  db.User.create({ email: "johndoe@web.site", firstName: "John", lastName: "Doe", password: "Hello" });
-  db.User.create({ email: "janedoe@web.site", firstName: "Jane", lastName: "Doe", password: "Howdy" });
+  db.User.create({
+    email: "johndoe@web.site",
+    firstName: "John",
+    lastName: "Doe",
+    password: "Hello",
+  });
+  db.User.create({
+    email: "janedoe@web.site",
+    firstName: "Jane",
+    lastName: "Doe",
+    password: "Howdy",
+  });
 };
