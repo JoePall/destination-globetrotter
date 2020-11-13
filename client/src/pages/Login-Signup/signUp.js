cea2a4af6394a61d633dc85f0f78bc9d4a5b72f3
Josiah Powell <josiahpowell@outlook.com>
2020-11-13 14:44:31 -0600

    Login styling

--- client/src/pages/Login-Signup/signUp.js
+++ client/src/pages/Login-Signup/signUp.js
@@ -36,24 +36,24 @@ class Signup extends React.Component {
   
   render() {
     return (
-      <div className="wrapper">
-        <div id="formContent" className="container">
+      <div className="wrapper my-5 p-5">
+        <div id="formContent" className="container px-3">
           <div className="row mx-auto my-2">
-            <a href="/login" className="col-5 mx-auto btn btn-white my-4 btn-lg m-1">login</a>
-            <a href="/signup" className="col-5 mx-auto btn btn-outline-dark my-4 btn-lg m-1">signup</a>
+            <a href="/login" className="col-5 mx-auto btn btn-outline-success my-4 btn-lg m-1">login</a>
+            <a href="/signup" className="col-5 mx-auto btn btn-outline-dark disabled my-4 btn-lg m-1">signup</a>
           </div>
 
           <div className="row mx-auto my-2">
             <img src={Logo} className="mx-auto" id="icon" alt="User Icon"></img>
           </div>
 
-          <div className="row mx-auto my-2">
+          <div className="row mx-auto my-2 fadeIn">
             <input type="email" className="mx-auto" onInput={this.handleInputChange} name="email" placeholder="email" />
           </div>
-          <div className="row mx-auto my-2">
+          <div className="row mx-auto my-2 fadeIn">
             <input type="password" className="mx-auto" onInput={this.handleInputChange} name="password" placeholder="password" />
           </div>
-          <div className="row mx-auto my-2">
+          <div className="row mx-auto my-2 fadeIn">
             <button onClick={this.handleSubmit} className="btn btn-primary my-4 w-75 mx-auto btn-lg">Signup</button>
           </div>
         </div>
