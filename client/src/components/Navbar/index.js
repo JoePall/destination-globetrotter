7c18c710c4dcffa59373dc65ed4b713407daf497
Andrew Popp <apopp524@gmail.com>
2020-11-12 20:08:06 -0600

    added icons to navbar

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -3,6 +3,12 @@ import { slide as Menu } from "react-burger-menu";
 import { Link } from "react-router-dom";
 import "./style.css";
 import Logo from "../../images/logo-small.png";
+import { FaUserAlt } from 'react-icons/fa';
+import { FaCommentDots } from 'react-icons/fa';
+import { FaSearchLocation } from 'react-icons/fa';
+import { FaSuitcaseRolling } from 'react-icons/fa';
+import { FaPlane } from 'react-icons/fa';
+import { FaHome } from 'react-icons/fa';
 
 function Navbar() {
   function showSettings(event) {
@@ -17,20 +23,23 @@ function Navbar() {
         </Link>
 
         <Menu>
+        <a className="menu-item" href="/">
+            <FaHome />Home
+          </a>
           <a className="menu-item" href="/my-flights">
-            My Flights
+            <FaPlane />My Flights
           </a>
           <a className="menu-item" href="/search-flights">
-            Search Flights
+          <FaSearchLocation />Search Flights
           </a>
           <a className="menu-item" href="/trips">
-            Trips
+          <FaSuitcaseRolling />Trips
           </a>
           <a className="menu-item" href="/messages">
-            Messages
+          <FaCommentDots />Messages
           </a>
           <a className="menu-item" href="/profile">
-            Profile
+          <FaUserAlt />Profile
           </a>
         </Menu>
       </header>
