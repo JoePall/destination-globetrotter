5cba03145ddcc56a36757d8aa548180a409989a7
cermqm <63324300+cermqm@users.noreply.github.com>
2020-11-16 23:04:31 -0600

    updated search-flights index.js

--- client/src/pages/Search-Flights/index.js
+++ client/src/pages/Search-Flights/index.js
@@ -8,36 +8,86 @@ import Form from 'react-bootstrap/Form'
 import Col from 'react-bootstrap/Col'
 import "./style.css"
 import DisplayFlight from "./DisplayFlight.js";
-
 import DatePicker from "react-datepicker"
 import "../../../node_modules/react-datepicker/dist/react-datepicker.css";
 import api from "../../utils/API";
+import Select from "react-select";
+
+const options = [
+  { value: 'ATL', label: 'ATL - Hartsfield–Jackson Atlanta International Airport' },
+  { value: 'BOS', label: 'BOS - General Edward Lawrence Logan International Airport' },
+  { value: 'BWI', label: 'BWI - Baltimore–Washington International Airport' },
+  { value: 'CLT', label: 'CLT - Charlotte Douglas International Airport' },
+  { value: 'DCA', label: 'DCA - Ronald Reagan Washington National Airport' },
+  { value: 'DEN', label: 'DEN - Denver International Airport' },
+  { value: 'DFW', label: 'DFW - Dallas/Fort Worth International Airport' },
+  { value: 'DTW', label: 'DTW - Detroit Metropolitan Airport' },
+  { value: 'EWR', label: 'EWR - Newark Liberty International Airport' },
+  { value: 'FLL', label: 'FLL - Fort Lauderdale–Hollywood International Airport' },
+  { value: 'HNL', label: 'HNL - Honolulu Hawaii International Airport' },
+  { value: 'IAD', label: 'IAD - Washington Dulles International Airport' },
+  { value: 'IAH', label: 'IAH - George Bush Intercontinental Airport' },
+  { value: 'JFK', label: 'JFK - John F. Kennedy International Airport' },
+  { value: 'LAS', label: 'LAS - McCarran International Airport' },
+  { value: 'LAX', label: 'LAX - Los Angeles International Airport' },
+  { value: 'LGA', label: 'LGA - LaGuardia Airport' },
+  { value: 'MCI', label: 'MCI - Kansas City International Airport' },
+  { value: 'MCO', label: 'MCO - Orlando International Airport' },
+  { value: 'MDW', label: 'MDW - Chicago Midway International Airport' },
+  { value: 'MIA', label: 'MIA - Miami International Airport' },
+  { value: 'MSP', label: 'MSP - Minneapolis–Saint Paul International Airport' },
+  { value: 'ORD', label: 'ORD - O\'Hare International Airport' },
+  { value: 'PDX', label: 'PDX - Portland International Airport' },
+  { value: 'PHL', label: 'PHL - Philadelphia International Airport' },
+  { value: 'PHX', label: 'PHX - Phoenix Sky Harbor International Airport' },
+  { value: 'SAN', label: 'SAN - San Diego International Airport' },
+  { value: 'SEA', label: 'SEA - Seattle–Tacoma International Airport' },
+  { value: 'SFO', label: 'SFO - San Francisco International Airport' },
+  { value: 'SLC', label: 'SLC - Salt Lake City International Airport' },
+  { value: 'TPA', label: 'TPA - Tampa International Airport' }
+];
+
+let sortby = "price";
 
 const Search = () => {
 
     const [fromairport, setfromairport] = useState('');
     const [toairport, settoairport] = useState('');
-    const [returnto, setreturnto] = useState(new Date());
-    const [dateto, setdateto] = useState(new Date());
+    const [returnto, setreturnto] = useState(false);
+    const [dateto, setdateto] = useState('');
     const [searchresults, setsearchresults] = useState(null);
     const [displayflights, setdisplayflights] = useState(null);
-    const [selectflight, setselectflight] = useState(null);
-
+    const [isLoading, setisLoading] = useState(false);
+    const [sortby, setsortby] = useState(null);
 
     useEffect(() => {
         const timeoutID = setTimeout(() => {
-            if (fromairport && toairport && dateto && returnto) {
+            if (fromairport && toairport && dateto && returnto && sortby) {
                 console.log(fromairport)
                 console.log(toairport)
                 console.log(returnto)
                 console.log(dateto)
-                Searchbar(returnto, dateto, fromairport, toairport, setsearchresults);
+                console.log("sortby = " + sortby);
+                setisLoading(true);
+                setsearchresults(null);
+                Searchbar(returnto, dateto, fromairport, toairport, setsearchresults, sortby);
             }
         }, 750);
         return () => {
-            clearTimeout(timeoutID);
+          clearTimeout(timeoutID);
+          setisLoading(false)
         };
-    }, [fromairport, toairport, dateto, returnto]);
+    }, [fromairport, toairport, dateto, returnto, sortby]);
+
+    const displayloading = () => {
+      // console.log("in displayloading");
+      return (<div className="loadingclass">Loading...</div>)
+    }
+
+    // const clearloading = () => {
+    //   console.log("clearloading");
+    //   return (<div></div>)
+    // }
 
     const selectflightfunction = (result) => {
       // setselectflight(result.id)
@@ -47,36 +97,63 @@ const Search = () => {
       api.bookmark.create({data: result}).then(response => {console.log(response); return(response)})
     }
 
+
+
         return (
           <div className="ui segment searchbar" id="searchbar">
           <div className="form">
             <form className="field">
+              <Form.Row>
+              <Col className="firstcolumn">            
+              <label>Sort By</label>
+              </Col>
+              <Col className="secondcolumn">
+              <fieldset>
+              <div className="radiobutton">
+                <input className="radio" type="radio" value="price" name="sortresby"  onChange={(e) => setsortby(e.target.value)}/>
+                <label for="price">Price</label>
+                <input className="radio" type="radio" value="duration" name="sortresby"  onChange={(e) => setsortby(e.target.value)}/>
+                <label for="duration">Duration</label>
+                <input className="radio" type="radio" value="quality" name="sortresby"  onChange={(e) => setsortby(e.target.value)}/>
+                <label for="quality">Quality</label>
+                <input className="radio" type="radio" value="date" name="sortresby"  onChange={(e) => setsortby(e.target.value)}/>
+                <label for="date">Date</label>
+              </div>
+              </fieldset>
+              </Col>
+              </Form.Row>
+              {/* <br /> */}
               <Form.Row> 
-              <Col>            
+              <Col className="firstcolumn">            
               <label>Departure Airport</label>
               </Col>
-              <Col> 
-              <input
-                placeholder="Airport Code - i.e. MCI"
-                value={fromairport}
-                onChange={(e) => setfromairport(e.target.value)}
-                className="input"
+              <Col className="secondcolumn"> 
+              <Select
+                 placeholder="Airport Code - i.e. MCI"
+                 options={options}
+                 onChange={(e) => setfromairport(e.value)}
+                 className="input"
               />
               </Col>
               </Form.Row>
               <Form.Row> 
-              <Col>            
+              <Col className="firstcolumn">            
               <label>Return Airport</label>
               </Col>
-              <Col> 
-              <input
-                placeholder="Airport Code - i.e. LAS"
-                value={toairport}
-                onChange={(e) => settoairport(e.target.value)}
-                className="input"
+              <Col className="secondcolumn"> 
+              <Select
+                 placeholder="Airport Code - i.e. LAX"
+                 options={options}
+                 onChange={(e) => settoairport(e.value)}
+                 className="input"
               />
               </Col>
               </Form.Row>
+              <Form.Row>
+              <Col className="firstcolumn">            
+              <label>Departure Date</label>
+              </Col>
+              <Col className="secondcolumn"> 
               <DatePicker
                 placeholder="Departure Date"
                 value={dateto}
@@ -86,7 +163,13 @@ const Search = () => {
                 onChange={date => setdateto(date)}
                 className="input"
               />
-              <br />
+              </Col>
+              </Form.Row>
+              <Form.Row>
+              <Col className="firstcolumn">            
+              <label>Return Date</label>
+              </Col>
+              <Col className="secondcolumn"> 
               <DatePicker
                 placeholder="Return Date"
                 value={returnto}
@@ -96,14 +179,18 @@ const Search = () => {
                 onChange={date => setreturnto(date)}
                 className="input"
               />
+              </Col>
+              </Form.Row>
                 {/* <button>Search</button> */}
         </form>
   </div>
-                    {
-                        searchresults &&
+
+                       { (isLoading && !searchresults) ? displayloading() : "" }
+                       { searchresults &&
                         searchresults.map( result => {
                             let departureduration = 0;
                             let returnduration = 0;
+                            // console.log("result inside of search flights = ", result)
                             let airlinename = ConvertAirline(result.airlines[0]);
                             let ddh = CalculateDuration(departureduration = result.duration.departure);
                             let rdh = CalculateDuration(returnduration = result.duration.return);
@@ -128,8 +215,8 @@ const Search = () => {
                                 </Table>
                                 
                                 {(displayflights === result.id) ? DisplayFlight(result={result}) : ""}
-                                {/* {(selectflight === result.id) ? api.bookmark.create({data: result}) : ""} */}
-                                {(selectflight === result.id) ? console.log("result = ", result) : ""}
+                                {/* {(selectflight === result.id) ? api.bookmarks.create({data: result}) : ""} */}
+                                {/* {(selectflight === result.id) ? console.log("result = ", result) : ""} */}
                                 </div>
                                 )
                             }
