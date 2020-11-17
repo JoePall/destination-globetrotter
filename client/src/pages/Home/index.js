f7d6bc856b4a01644ff748a0db3f2fadcb13325c
Josiah Powell <josiahpowell@outlook.com>
2020-11-17 14:48:42 -0600

    Updating Home page!

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,30 +1,56 @@
-import React, { useState, useEffect } from "react";
-import api from "../../utils/API";
+import React, { useState } from "react";
 import Logo from "../../images/better-logo.png";
-import { Container } from "react-bootstrap";
+import { Button, Card, Col, Container, Row } from "react-bootstrap";
 import "./style.css";
+import Select from "react-select";
+import options from "../../utils/codes.json";
+import { useHistory } from "react-router-dom";
+
 function Home() {
-  const [bookmarks, setBookmarks] = useState([]);
-
-  useEffect(() => {
-    loadBookmarks();
-  }, [bookmarks]);
-
-  function loadBookmarks() {
-    api.bookmark.get().then(res => {
-      console.log(res);
-      
-      setBookmarks(JSON.stringify(res.data));
-    }).catch(console.log);
-  }
+  const history = useHistory();
+  let location = "";
 
   return (
     <Container fluid="lg" className="p-4">
-      
-      <div class="container">
-      <h1 class="animate__animated animate__fadeIn">Welcome to</h1>
-        <img src={Logo} alt="Destination Globetrotter logo" class="animate__animated animate__fadeInUpBig"></img>
-      </div>
+      <Card className="animate__animated animate__fadeInUpBig text-center mx-auto p-5 m-5">
+        <h1 className="animate__animated animate__fadeIn mx-auto">Welcome to</h1>
+        <img
+          src={Logo}
+          alt="Destination Globetrotter logo"
+          className="mx-auto"
+        ></img>
+        <h2 className="text-center mx-auto">Grab a friend and let's go Places!</h2>
+      </Card>
+
+      <Row>
+        <Col>
+          <Select
+            placeholder=" Going places?"
+            options={options.map((option) => {
+              return {
+                value: option[option.length - 1],
+                label: option[0].trim() + ", " + option[1].trim() + (option.length === 2 ? option[2] : "") + " - " + option[option.length - 1], 
+                search: option[0].split(" ")[0]
+              };
+            })}
+            className="input"
+            onChange={(e) => {
+              console.log(e);
+              location = e;
+            }}
+          />
+        </Col>
+        <Button onClick={() => {
+          history.push({
+            pathname: '/trips',
+            state: {
+              location: location
+            }
+          });
+        }}>
+          Let's go Places!
+        </Button>
+      </Row>
     </Container>
   );
 }
