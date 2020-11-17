a80dcdee4a0acf8600143d1c2896d2bf34e6cb33
Julie Schaub <julie.schaub@hotmail.com>
2020-11-16 19:51:58 -0600

    updated profile

--- client/src/pages/Profile/index.js
+++ client/src/pages/Profile/index.js
@@ -1,19 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import Avatar from 'react-avatar';
-import './style.css';
-
+import "./style.css";
 
 function Profile() {
+
   const [user] = useState(JSON.parse(sessionStorage.getItem("user")));
+  const initials = (user.firstName + user.lastName);
 
   return (
     <div className="form">
       <form className="field">
       <div className="card-content">
 
-      <Avatar size="125" round={true} color={Avatar.getRandomColor('sitebase', ['green', 'purple', 'orange', 'red', 'aqua', 'blue', 'yellow'])} name={user.firstName + user.lastName} />
+      <Avatar size="125" round={true} color={Avatar.getRandomColor('sitebase', ['green', 'purple', 'orange', 'red', 'aqua', 'blue', 'yellow'])} name={initials} />
           <h2 id="name" className="">{user.firstName + " " + user.lastName}</h2>
-                <p><i className=""></i>{user.email}</p>
+                <p><i className=""></i> {user.email}</p>
                 <p><i className=""></i> Group Chat: {user.firstName + " " + user.lastName}</p>
       </div>
   </form>
