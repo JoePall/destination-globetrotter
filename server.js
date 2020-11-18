ae431e38f6a535c9f3112369a6130c375980497c
Josiah Powell <josiahpowell@outlook.com>
2020-11-17 19:22:51 -0600

    Heroku v2.1

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ db.sequelize.sync({ force: true }).then((seq) => {
 
   // Always return the main index.html, so react-router render the route in the client
   app.get('/*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'index.html'));
   });
 
   
