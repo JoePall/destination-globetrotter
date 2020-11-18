f9f144a504ee1edaef091850dd017da2fe5669a4
Josiah Powell <josiahpowell@outlook.com>
2020-11-18 00:56:41 -0600

    Code Refactor

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-const path = require("path");
 
 const session = require("express-session");
 const passport = require("./config/passport");
