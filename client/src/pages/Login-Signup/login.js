ca8d0e05f2ddc27e4ba86f6ccc97a2077e37c89c
Josiah Powell <josiahpowell@outlook.com>
2020-11-18 01:24:01 -0600

    Maintaining route during login

--- client/src/pages/Login-Signup/login.js
+++ client/src/pages/Login-Signup/login.js
@@ -24,7 +24,7 @@ class Login extends React.Component {
     api.user.login(this.state)
       .then(res => {
         sessionStorage.setItem("user", JSON.stringify(res.data));
-        window.location.assign("/");
+        location.reload();
       })
       .catch(console.log);
   }
