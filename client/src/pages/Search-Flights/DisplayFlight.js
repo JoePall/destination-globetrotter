d527b8b9a6ec6d6ed9f68af43927fad2e41e41a2
Josiah Powell <josiahpowell@outlook.com>
2020-11-19 09:36:03 -0600

    Adding reusable styling for displaying flights

--- client/src/pages/Search-Flights/DisplayFlight.js
+++ client/src/pages/Search-Flights/DisplayFlight.js
@@ -1,39 +1,48 @@
 import React from "react";
-import Card from 'react-bootstrap/Card';
+import Card from "react-bootstrap/Card";
+import Row from "react-bootstrap/Row";
 import ConvertAirline from "./ConvertAirline";
 import CalculateDuration from "./CalculateDuration";
-import ConvertDateTime  from "./ConvertDateTime";
-
+import ConvertDateTime from "./ConvertDateTime";
+import Col from "react-bootstrap/Col";
 
 function DisplayFlight(props) {
+
+
+  if (!props.result && props.result.route.length === 0) return null;
+  console.log(props.result);
+
+  return (
+    <Row>
+        <Col lg={6} md={12}>
+            <Card className="border border-warning m-3 p-3">
+                <h3>Return Flight</h3>
+                {props.result.route.filter(r => r.return === 0).map((route, i) => getFlight(route, i, props))}
+            </Card>
+        </Col>
+        <Col lg={6} md={12}>
+            <Card className="border border-warning m-3 p-3">
+                <h3>Destination Flight</h3>
+                {props.result.route.filter(r => r.return === 1).map((route, i) => getFlight(route, i, props))}
+            </Card>
+        </Col>
+    </Row>
+  );
+}
+
+function getFlight(route, i, props) {
     let durationdeparture = props.result.duration.departure;
     let durationreturn = props.result.duration.return;
     let ddh = CalculateDuration(durationdeparture);
     let rdh = CalculateDuration(durationreturn);
 
-    if (!props.result && props.result.route.length === 0) return (null)
-    return (
-        <div>
-            <Card className="mx-auto my-3 col-9">
-                <Card.Img variant="top" src="" />
-                <Card.Body>
-                    {props.result.route.map((route, i) => (
-                        <div key={i}>
-                            <Card.Title className="cardtitle">Flight Detail</Card.Title>
-                                <Card.Text className="cardtext">
-                                    <div>{ConvertAirline(props.result.route[i].airline)} - Flight #{props.result.route[i].flight_no}</div>
-                                    <div>{props.result.route[i].cityFrom} -- Local Departure - {ConvertDateTime(props.result.route[i].local_departure)}</div>
-                                    <div>{props.result.route[i].cityTo} -- Local Arrival - {ConvertDateTime(props.result.route[i].local_arrival)}</div>
-                                    <div>{(props.result.cityCodeFrom === props.result.route[i].cityCodeFrom) ? ddh : ""}</div>
-                                    <div>{(props.result.cityCodeTo === props.result.route[i].cityCodeFrom) ? rdh : ""}</div>
-                                    <div className="flightdetaildivider">{(props.result.cityCodeTo === props.result.route[i].cityCodeTo) ? "****************************" : ""}</div>
-                                </Card.Text>
-                        </div>
-                    ))}
-                </Card.Body>
-            </Card>
-        </div>
-    )
+     return <div key={i} className="my-3">
+        <div>{ConvertAirline(props.result.route[i].airline)} - Flight #{props.result.route[i].flight_no}</div>
+        <div>{props.result.route[i].cityFrom} -- Local Departure - {ConvertDateTime(props.result.route[i].local_departure)}</div>
+        <div>{props.result.route[i].cityTo} -- Local Arrival - {ConvertDateTime(props.result.route[i].local_arrival)}</div>
+        <div>{(props.result.cityCodeFrom === props.result.route[i].cityCodeFrom) ? ddh : ""}</div>
+        <div>{(props.result.cityCodeTo === props.result.route[i].cityCodeFrom) ? rdh : ""}</div>
+    </div>
 }
 
-export default DisplayFlight
+export default DisplayFlight;
