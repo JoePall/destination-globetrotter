c1342f38eb3886cfc5e8cdb2dd4b1210ac52cecf
Josiah Powell <josiahpowell@outlook.com>
2020-11-19 09:18:38 -0600

    More work on trips

--- client/src/pages/Trips/TripDashboard.js
+++ client/src/pages/Trips/TripDashboard.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Card, Container, Col, Row } from "react-bootstrap";
 import "./style.css";
 import apiHandler from "../../utils/apiHandler";
+import DisplayFlight from "../Search-Flights/DisplayFlight";
 
 function TripDashboard(id) {
   return (
@@ -11,16 +12,10 @@ function TripDashboard(id) {
         <h3>Bookmarks</h3>
         {apiHandler({
           path: "/api/bookmarksbytrip/" + id,
-          component: (response) => (
-            <Card bg="light" className="col-7 mx-auto">
-              {console.log(response.response)}
-              {response.response.map((item) => (
-                <span key={item.id} className="btn btn-primary rounded-pill mx-auto p-2 m-2">
-                  {JSON.stringify(item)}
-                </span>
-              ))}
-            </Card>
-          ),
+          component: (response) => {
+            console.log(response);
+            return DisplayFlight({result: response.response[0].data});
+          }
         })}
         </Col> 
         <Col>
