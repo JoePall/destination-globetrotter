b27535d0a40d1c40c5b76c3d43f1d0c99d90cf0b
cermqm <63324300+cermqm@users.noreply.github.com>
2020-11-20 11:38:34 -0600

    fixed display flight - again - lol...

--- client/src/pages/Search-Flights/DisplayFlight.js
+++ client/src/pages/Search-Flights/DisplayFlight.js
@@ -13,43 +13,22 @@ function DisplayFlight(props) {
   if (!props.result && props.result.route.length === 0) return null;
   console.log(props.result);
 
-  if (props.result.route.length === 2) {
-        return (
-            <Row>
-                <Col lg={6} md={12}>
-                    <Card className="border border-warning m-3 p-3">
-                        <h3>Departure Flight</h3>
-                        {props.result.route.filter(r => r.return === 0).map((route, i) => getFlight(route, i, props))}
-                    </Card>
-                </Col>
-                <Col lg={6} md={12}>
-                    <Card className="border border-warning m-3 p-3">
-                        <h3>Return Flight</h3>
-                        {props.result.route.filter(r => r.return === 1).map((route, i) => getFlight(route, i+1, props))}
-                    </Card>
-                </Col>
-            </Row>
-        )
-    }
-
-    if (props.result.route.length === 4) {
-        return (
-            <Row>
-                <Col lg={6} md={12}>
-                    <Card className="border border-warning m-3 p-3">
-                        <h3>Departure Flight</h3>
-                        {props.result.route.filter(r => r.return === 0).map((route, i) => getFlight(route, i, props))}
-                    </Card>
-                </Col>
-                <Col lg={6} md={12}>
-                    <Card className="border border-warning m-3 p-3">
-                        <h3>Return Flight</h3>
-                        {props.result.route.filter(r => r.return === 1).map((route, i) => getFlight(route, i+2, props))}
-                    </Card>
-                </Col>
-            </Row>
-        )
-    }
+return (
+    <Row>
+        <Col lg={6} md={12}>
+            <Card className="border border-warning m-3 p-3">
+                <h3>Departure Flight</h3>
+                {props.result.route.filter(r => r.return === 0).map((route, i) => getFlight(route, i, props))}
+            </Card>
+        </Col>
+        <Col lg={6} md={12}>
+            <Card className="border border-warning m-3 p-3">
+                <h3>Return Flight</h3>
+                {props.result.route.filter(r => r.return === 1).map((route, i) => getFlight(route, i+1, props))}
+            </Card>
+        </Col>
+    </Row>
+)
 }
 
 function getFlight(route, i, props) {
@@ -61,11 +40,11 @@ function getFlight(route, i, props) {
     let rdh = CalculateDuration(durationreturn);
 
      return <div key={i} className="my-3">
-        <div>{ConvertAirline(props.result.route[i].airline)} - Flight #{props.result.route[i].flight_no}</div>
-        <div>{props.result.route[i].cityFrom} -- Local Departure - {ConvertDateTime(props.result.route[i].local_departure)}</div>
-        <div>{props.result.route[i].cityTo} -- Local Arrival - {ConvertDateTime(props.result.route[i].local_arrival)}</div>
-        <div>{(props.result.cityCodeFrom === props.result.route[i].cityCodeFrom) ? ddh : ""}</div>
-        <div>{(props.result.cityCodeTo === props.result.route[i].cityCodeFrom) ? rdh : ""}</div>
+        <div>{ConvertAirline(route.airline)} - Flight #{route.flight_no}</div>
+        <div>{route.cityFrom} -- Local Departure - {ConvertDateTime(route.local_departure)}</div>
+        <div>{route.cityTo} -- Local Arrival - {ConvertDateTime(route.local_arrival)}</div>
+        <div>{(props.result.cityCodeFrom === route.cityCodeFrom) ? ddh : ""}</div>
+        <div>{(props.result.cityCodeTo === route.cityCodeFrom) ? rdh : ""}</div>
     </div>
 }
 
