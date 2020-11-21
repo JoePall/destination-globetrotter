0db52935ac03a6fca5879993014c0314a6ffa26c
Josiah Powell <josiahpowell@outlook.com>
2020-11-21 00:31:55 -0600

    Working

--- client/src/pages/Friends/index.js
+++ client/src/pages/Friends/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
 import Pending from './components/pending';
 
 const Friends = () => {
   return (
-    <Container fluid>
+    <Container fluid className="mx-auto text-center mb-3">
+      <h1>Requests</h1>
       <Pending />
     </Container>);
 };
