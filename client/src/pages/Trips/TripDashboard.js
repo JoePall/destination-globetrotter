77889811965419abde5ddd921cbf4b66a52b25ac
Josiah Powell <josiahpowell@outlook.com>
2020-12-05 21:37:15 -0600

    Adding nearby and itinerary to trip dashboard

--- client/src/pages/Trips/TripDashboard.js
+++ client/src/pages/Trips/TripDashboard.js
@@ -4,6 +4,8 @@ import Messages from "./components/Messages";
 import Bookmarks from "./components/Bookmarks";
 import People from "./components/People";
 import TripItem from "./TripItem";
+import Nearby from "../../components/Nearby";
+import TripCalendar from "./components/TripCalendar";
 
 function TripDashboard(props) {
   return (
@@ -24,10 +26,18 @@ function TripDashboard(props) {
           <Messages tripId={props.tripId} />
         </Col>
       </Row>
+      <Row>
+        <Col md={12} lg={6}>
+          <Nearby tripId={props.tripId} />
+        </Col>
+      </Row>
+      <Row className="bg-light my-3 py-3">
+        <Col md={12} lg={12} className="m-0 p-0">
+          <TripCalendar tripId={props.tripId} />
+        </Col>
+      </Row>
     </Container>
   );
 }
 
 export default TripDashboard;
-
-
