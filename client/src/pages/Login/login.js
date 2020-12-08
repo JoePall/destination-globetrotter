208f548bd0c60a7629b385efdc5182a6ef9b0b44
Josiah Powell <josiahpowell@outlook.com>
2020-12-08 11:01:10 -0600

    Adding nice buttons

--- client/src/pages/Login/login.js
+++ client/src/pages/Login/login.js
@@ -2,16 +2,17 @@ import React from "react";
 import "./style.css";
 import Logo from "../../images/logo-small.png";
 import api from "../../utils/API";
+import TextField from "@material-ui/core/TextField";
 
 class Login extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       email: "",
-      password: ""
+      password: "",
     };
-    
-    this.loaded = false; 
+
+    this.loaded = false;
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.loaded = true;
@@ -20,9 +21,10 @@ class Login extends React.Component {
   handleSubmit(event) {
     event.preventDefault();
     if (!this.loaded) return;
-    
-    api.user.login(this.state)
-      .then(res => {
+
+    api.user
+      .login(this.state)
+      .then((res) => {
         sessionStorage.setItem("user", JSON.stringify(res.data));
         location.reload();
       })
@@ -31,17 +33,27 @@ class Login extends React.Component {
 
   handleInputChange({ target }) {
     this.setState({
-      [target.name]: target.type === 'checkbox' ? target.checked : target.value
+      [target.name]: target.type === "checkbox" ? target.checked : target.value,
     });
   }
-  
+
   render() {
     return (
       <div className="wrapper my-5 p-5">
         <div id="formContent" className="container px-3">
           <div className="row mx-auto my-2">
-            <a href="/login" className="col-5 mx-auto btn btn-outline-dark disabled my-4 btn-lg m-1">login</a>
-            <a href="/signup" className="col-5 mx-auto btn btn-outline-primary my-4 btn-lg m-1">signup</a>
+            <a
+              href="/login"
+              className="col-5 mx-auto btn btn-outline-dark disabled my-4 btn-lg m-1"
+            >
+              login
+            </a>
+            <a
+              href="/signup"
+              className="col-5 mx-auto btn btn-outline-primary my-4 btn-lg m-1"
+            >
+              signup
+            </a>
           </div>
 
           <div className="row mx-auto my-2">
@@ -49,14 +61,44 @@ class Login extends React.Component {
           </div>
 
           <div className="row mx-auto my-2 fadeIn">
-            <input type="email" className="mx-auto" onInput={this.handleInputChange} name="email" placeholder="email" />
-
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              type="email"
+              className="mx-auto"
+              onInput={this.handleInputChange}
+            />
           </div>
           <div className="row mx-auto my-2 fadeIn">
-            <input type="password" className="mx-auto" onInput={this.handleInputChange} name="password" placeholder="password" />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              className="mx-auto"
+              onInput={this.handleInputChange}
+            />
           </div>
           <div className="row mx-auto my-2 fadeIn">
-            <button autoFocus onClick={this.handleSubmit} className="btn btn-success my-4 w-75 mx-auto btn-lg">Login</button>
+            <button
+              autoFocus
+              onClick={this.handleSubmit}
+              className="btn btn-success my-4 w-75 mx-auto btn-lg"
+            >
+              Login
+            </button>
           </div>
         </div>
       </div>
