88e47121bda2ae2abf73a2ca120b32217c6533c4
Josiah Powell <josiahpowell@outlook.com>
2020-12-08 21:46:30 -0600

    Refactor

--- server.js
+++ server.js
@@ -22,10 +22,10 @@ app.use(
 );
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(requireHTTPS);
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
+  app.use(requireHTTPS);
   app.use(express.static("client/build"));
 }
 
@@ -71,34 +71,34 @@ const seed = () => {
   db.pending.create({
     requestedId: 1,
     requesterId: 2,
-    tripId: 2,
+    tripId: "b6d4fad9-1d75-4870-9363-9c550fa59ec6",
   });
 
   db.pending.create({
     requestedId: 2,
     requesterId: 1,
-    tripId: 2,
+    tripId: "b6d4fad9-1d75-4870-9363-9c550fa59ec6",
   });
   db.group.create({
     name: "KU Coding Bootcamp Class of '20",
   });
   db.trip_user.create({
     userId: 1,
-    tripId: 1,
+    tripId: "177c261f-1b15-41b1-bdd8-bbb9cb6ad227",
   });
   db.group.create({
     name: "Smith Family",
   });
   let date = Date.now();
   db.trip.create({
-    id: 2,
+    id: 1,
     location: "Los Angeles",
     start: moment(date).add(145, "days").format("M/D/Y"),
     end: moment(date).add(150, "days").format("M/D/Y"),
     userId: 1,
   });
   db.trip.create({
-    id: 1,
+    id: "177c261f-1b15-41b1-bdd8-bbb9cb6ad227",
     location: "Los Angeles",
     start: moment(date).add(410, "days").format("M/D/Y"),
     end: moment(date).add(415, "days").format("M/D/Y"),
@@ -113,8 +113,24 @@ const seed = () => {
     groupId: 3,
   });
   db.trip_user.create({
-    tripId: 1,
+    tripId: "177c261f-1b15-41b1-bdd8-bbb9cb6ad227",
     userId: 1,
+  });
+  db.trip_user.create({
+    tripId: "177c261f-1b15-41b1-bdd8-bbb9cb6ad227",
+    userId: 2,
+  });
+  db.trip_user.create({
+    tripId: "177c261f-1b15-41b1-bdd8-bbb9cb6ad227",
+    userId: 3,
+  });
+  db.trip_user.create({
+    tripId: "177c261f-1b15-41b1-bdd8-bbb9cb6ad227",
+    userId: 4,
+  });
+  db.trip_user.create({
+    tripId: "177c261f-1b15-41b1-bdd8-bbb9cb6ad227",
+    userId: 5,
   });
   db.user.create({
     email: "josiahpowell@outlook.com",
@@ -124,25 +140,25 @@ const seed = () => {
   });
   db.message.create({
     userId: 1,
-    tripId: 1,
+    tripId: "177c261f-1b15-41b1-bdd8-bbb9cb6ad227",
     text:
       "Fly into Hawaii and jump on a cruise? ... we can see every island that way and unload our bags once as opposed to multiple hotels on different islands and extra travel time which can be accomplished during the night.",
   });
   db.message.create({
     userId: 2,
-    tripId: 1,
+    tripId: "177c261f-1b15-41b1-bdd8-bbb9cb6ad227",
     text: "That sounds good!",
   });
   
   db.event.create({
-    tripId: 1,
+    tripId: "177c261f-1b15-41b1-bdd8-bbb9cb6ad227",
     userId: 1,
     title: "Josiah Powell @Knott's Berry Farm",
     start: moment(date).add(412, "days").format("YYYY-MM-DD") + "T13:30:00",
     end: moment(date).add(412, "days").format("YYYY-MM-DD") + "T16:30:00",
   });
   db.event.create({
-    tripId: 1,
+    tripId: "177c261f-1b15-41b1-bdd8-bbb9cb6ad227",
     userId: 1,
     title: "Josiah Powell @Huntington Beach (Surf City)",
     start: moment(date).add(413, "days").format("YYYY-MM-DD"),
@@ -150,7 +166,7 @@ const seed = () => {
   });
   
   db.event.create({
-    tripId: 2,
+    tripId: "b6d4fad9-1d75-4870-9363-9c550fa59ec6",
     userId: 2,
     title: "John Doe @DisneyLand",
     start: moment(date).add(147, "days").format("YYYY-MM-DD") + "T13:30:00",
@@ -158,7 +174,7 @@ const seed = () => {
   });
   
   db.event.create({
-    tripId: 2,
+    tripId: "b6d4fad9-1d75-4870-9363-9c550fa59ec6",
     userId: 2,
     title: "Julie Schaub @DisneyLand",
     start: moment(date).add(148, "days").format("YYYY-MM-DD") + "T09:30:00",
