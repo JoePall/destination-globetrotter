9c0d09698a65a75e329ae34247d062cb092b7b16
Josiah Powell <josiahpowell@outlook.com>
2020-12-09 22:54:30 -0600

    Refactor UI

--- client/src/pages/Trips/TripDashboard.js
+++ client/src/pages/Trips/TripDashboard.js
@@ -13,7 +13,7 @@ function TripDashboard(props) {
       <Row className="mb-5 mx-auto text-center">
         <TripItem tripId={props.tripId} />
       </Row>
-      <Row className="bg-light my-3 py-3">
+      <Row className="bg-warning my-3 py-3">
         <Col md={12} className="m-0 p-0">
           <Bookmarks tripId={props.tripId} />
         </Col>
@@ -32,7 +32,7 @@ function TripDashboard(props) {
           <Nearby tripId={props.tripId} />
         </Col>
       </Row>
-      <Row className="bg-light my-3 py-3">
+      <Row className="my-3 py-3">
         <Col md={12} className="m-0 p-0">
           <TripCalendar tripId={props.tripId} />
         </Col>
