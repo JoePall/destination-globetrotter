ae67a8804b38c1a615d0c169f552fca0aa546cd2
Josiah Powell <josiahpowell@outlook.com>
2020-12-09 19:52:36 -0600

    UI refactor

--- client/src/pages/Friends/components/pending.js
+++ client/src/pages/Friends/components/pending.js
@@ -2,6 +2,7 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import api from "../../../utils/API";
+import { Card } from "@material-ui/core";
 
 class Pending extends React.Component {
   constructor(props) {
@@ -21,53 +22,63 @@ class Pending extends React.Component {
   }
 
   render() {
-    return (
-      <Container fluid>
-        {this.state.pends.map((pend) => {
-          return (
-            <Container key={pend.pending.id} fluid>
-              <span className="mx-2">
-                {pend.owner.firstName +
-                  " " +
-                  pend.owner.lastName +
-                  " has invited you!"}
-              </span>
-              <span>{pend.trip.location}</span>
-              <span>
-                <Button
-                  className="btn btn-success m-2"
-                  onClick={() => {
-                    api.trip_user.create({
-                      userId: pend.pending.requestedId,
-                      tripId: pend.trip.id,
-                    });
-                    api.pending.delete(pend.pending.id);
-                    location.assign("/trips/" + pend.trip.id);
-                  }}
-                >
-                  Accept
-                </Button>
-                <Button
-                  className="btn btn-danger m-2"
-                  onClick={() => {
-                    api.pending.delete(pend.pending.id);
+    if (this.state.pends.length === 0) {
+      return (
+        <Container>
+          <Card className="p-3">
+            <h4>Sorry, no invites yet!</h4>
+          </Card>
+        </Container>
+      );
+    } else {
+      return (
+        <Container fluid>
+          {this.state.pends.map((pend) => {
+            return (
+              <Container key={pend.pending.id} fluid>
+                <span className="mx-2">
+                  {pend.owner.firstName +
+                    " " +
+                    pend.owner.lastName +
+                    " has invited you!"}
+                </span>
+                {/* <span>{pend.trip.location}</span> */}
+                <span>
+                  <Button
+                    className="btn btn-success m-2"
+                    onClick={() => {
+                      api.trip_user.create({
+                        userId: pend.pending.requestedId,
+                        tripId: pend.trip.id,
+                      });
+                      api.pending.delete(pend.pending.id);
+                      location.assign("/trips/" + pend.trip.id);
+                    }}
+                  >
+                    Accept
+                  </Button>
+                  <Button
+                    className="btn btn-danger m-2"
+                    onClick={() => {
+                      api.pending.delete(pend.pending.id);
 
-                    //TODO:
-                    this.setState({
-                      pends: this.state.pends.filter(
-                        (p) => p.pending.id !== pend.pending.id
-                      ),
-                    });
-                  }}
-                >
-                  Reject
-                </Button>
-              </span>
-            </Container>
-          );
-        })}
-      </Container>
-    );
+                      //TODO:
+                      this.setState({
+                        pends: this.state.pends.filter(
+                          (p) => p.pending.id !== pend.pending.id
+                        ),
+                      });
+                    }}
+                  >
+                    Reject
+                  </Button>
+                </span>
+              </Container>
+            );
+          })}
+        </Container>
+      );
+    }
   }
 }
 
